73969d286b8bd6413f147a1314732aeb375cce36
FabrizioFornone <fabrizio.fornone@digitware.it>
2024-09-21 13:26:39 +0200

    main router implementation

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import express, { Express } from "express";
 import dotenv from "dotenv";
-import { databaseConnection } from"./config/database";
+import { databaseConnection } from "./config/database";
+import { router } from "./routes/router";
 
 dotenv.config();
 
@@ -8,6 +9,9 @@ const app: Express = express();
 const port = process.env.PORT || 3000;
 
 app.use(express.json());
+
+//routes
+app.use("/api", router);
 
 const start = async (): Promise<void> => {
   try {
