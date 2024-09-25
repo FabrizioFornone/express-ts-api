c54f6a5c30eb5da8e9d657ba36875f9f44ca6387
FabrizioFornone <fabrizio.fornone@digitware.it>
2024-09-25 11:04:30 +0200

    tests improved

--- src/tests/main.test.ts
+++ src/tests/main.test.ts
@@ -29,6 +29,14 @@ describe("Endpoints", () => {
     expect(res.body).toHaveProperty("username");
   });
 
+  it("should return error username already exists", async () => {
+    const res = await request(app).post("/api/users/register").send({
+      username: "testuserr@gmail.com",
+      password: "password123",
+    });
+    expect(res.statusCode).toEqual(409);
+  });
+
   it("should return a read_write token for valid credentials", async () => {
     const res = await request(app)
       .post("/api/users/token")
@@ -41,6 +49,19 @@ describe("Endpoints", () => {
     expect(res.body).toHaveProperty("token");
   });
 
+  it("should return an error for invalid credentials", async () => {
+    const res = await request(app)
+      .post("/api/users/token")
+      .set(
+        "Authorization",
+        "Basic " +
+          Buffer.from("wrongtestuserr@gmail.com:wrongpassword123").toString(
+            "base64"
+          )
+      );
+    expect(res.statusCode).toEqual(401);
+  });
+
   it("should return a read token for valid credentials", async () => {
     const res = await request(app).post("/api/users/token");
     expect(res.statusCode).toEqual(201);
@@ -50,20 +71,35 @@ describe("Endpoints", () => {
   it("should get all investments", async () => {
     const res = await request(app)
       .get("/api/investments")
-      .set("Authorization", "Bearer token1");
+      .set("Authorization", "Bearer read_token1");
     expect(res.statusCode).toEqual(200);
     expect(res.body).toHaveProperty("data");
     expect(Array.isArray(res.body.data)).toBe(true);
     expect(res.body.data.length).toBeGreaterThan(0);
   });
 
+  it("should return an error 401: Invalid token", async () => {
+    const res = await request(app)
+      .get("/api/investments")
+      .set("Authorization", "Bearer wrongtoken");
+    expect(res.statusCode).toEqual(401);
+  });
+
+  it("should return an error 403: Token already used", async () => {
+    const res = await request(app)
+      .get("/api/investments")
+      .set("Authorization", "Bearer read_token1");
+    expect(res.statusCode).toEqual(403);
+  });
+
   it("should create a new investment", async () => {
     const res = await request(app)
       .post("/api/investments")
-      .set("Authorization", "Bearer token2")
+      .set("Authorization", "Bearer read_write_token2")
       .send({
         value: "5000",
         annual_rate: "5.5",
+        creation_date: "1727253312",
       });
     expect(res.statusCode).toEqual(201);
     expect(res.body).toHaveProperty("data");
@@ -72,13 +108,33 @@ describe("Endpoints", () => {
     expect(res.body.data).toHaveProperty("creation_date");
   });
 
+  it("should return an error for validation", async () => {
+    const res = await request(app)
+      .post("/api/investments")
+      .set("Authorization", "Bearer read_write_token4")
+      .send({
+        annual_wrong_prop: "2.1",
+        creation_wrongProp: "5427253312",
+      });
+    expect(res.statusCode).toEqual(400);
+  });
+
   it("should get investment metrics", async () => {
     const from = Math.floor(new Date("2024-01-01").getTime() / 1000);
     const to = Math.floor(new Date("2024-12-31").getTime() / 1000);
     const res = await request(app)
       .get(`/api/investments/metrics/?from=${from}&to=${to}&groupBy=month`)
-      .set("Authorization", "Bearer token3");
+      .set("Authorization", "Bearer read_token3");
     expect(res.statusCode).toEqual(200);
     expect(res.body).toBeInstanceOf(Object);
+  });
+
+  it("should return an error for validation: the to date must be >= to the from date", async () => {
+    const to = Math.floor(new Date("2024-01-01").getTime() / 1000);
+    const from = Math.floor(new Date("2024-12-31").getTime() / 1000);
+    const res = await request(app)
+      .get(`/api/investments/metrics/?from=${from}&to=${to}&groupBy=month`)
+      .set("Authorization", "Bearer read_token5");
+    expect(res.statusCode).toEqual(400);
   });
 });
