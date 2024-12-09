78e3c4dc532191bba7b134e652bcf2763f293dad
Fabrizio Fornone <91203979+FabrizioFornone@users.noreply.github.com>
2024-12-09 12:51:59 +0100

    npx knex local commands fix (#10)

--- knexfile.ts
+++ knexfile.ts
@@ -1,4 +1,3 @@
-import Knex from "knex";
 import dotenv from "dotenv";
 
 dotenv.config();
@@ -17,24 +16,22 @@ if (env) {
 }
 
 export default {
-  development: {
-    client: "mysql2",
-    connection: {
-      host: process.env.DB_HOST,
-      port: parseInt(process.env.DB_PORT!, 10),
-      user: process.env.DB_USER,
-      password: process.env.DB_PASSWORD,
-      database: process.env.DB_NAME,
-    },
-    pool: {
-      min: 2,
-      max: 10,
-    },
-    migrations: {
-      directory: "./src/migrations",
-    },
-    seeds: {
-      directory: "./src/seeds",
-    },
+  client: "mysql2",
+  connection: {
+    host: process.env.DB_HOST,
+    port: parseInt(process.env.DB_PORT!, 10),
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+  },
+  pool: {
+    min: 2,
+    max: 10,
+  },
+  migrations: {
+    directory: "./src/migrations",
+  },
+  seeds: {
+    directory: "./src/seeds",
   },
 };
